92a67091ebc3ab40bc572cd3f8a2f20c9b378f35
Tater <haleymariehilton@gmail.com>
2022-06-08 16:48:07 -0700

    wrote all pathways, began writing function for posting notes

--- server.js
+++ server.js
@@ -12,8 +12,34 @@ app.use('/api', api);
 app.use(express.static('public'));
 
 //GET /notes >> notes.html
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
-//GET * >> index.html
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+//GET * >> index.html (in lieu of 404 route)
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 
 //GET /api/notes read db.json
 //POST /api/notes add to db.json
+app.post('/api/notes', (req, res) => {
+    const newNote = createNote(req.body, allNotes);
+    res.json(newNote);
+});
+// GET Route for index.html
+
+
+
+function createNote() {
+    const newNote = body;
+}
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
