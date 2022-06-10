43e1c2829f67129e4429a498c7784c156d45f6a8
Tater <haleymariehilton@gmail.com>
2022-06-09 19:58:05 -0700

    added unique IDs and installed UUID

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,37 +1,37 @@
 const fs = require("fs");
 const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 
 
 
     // API GET Request
     router.get("/api/notes", (req, res) => {
         
-        // Read 'db.json' file 
         let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
         console.log("\nGET request - Returning notes data: " + JSON.stringify(data));
         
-        // Send read data to response of 'GET' request
         res.json(data);
     });
 
 
     // API POST Request
     router.post("/api/notes", (req, res) => {
-
+        
         const newNote = req.body;
         console.log("\n\nPOST request - New Note : " + JSON.stringify(newNote));
-
-        // Read data from 'db.json' file
+        newNote.id = uuidv4();
         let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
          data.push(newNote);
 
-        // Written notes data to 'db.json' file
         fs.writeFileSync('./db/db.json', JSON.stringify(data));
-        
         console.log("\nSuccessfully added new note to 'db.json' file!");
 
-        // Send response
         res.json(data);
     });
 
+    // API DELETE
+    router.delete("/api/notes/:id", (req, res) => {
+            const id = req.params.id
+
+    })
     module.exports = router;
